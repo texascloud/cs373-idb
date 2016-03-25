946d9d517e81f3bcf00eb33db70855d5e79f55cb
CamelBackNotation <joshuahurt@utexas.edu>
2016-03-24 21:12:33 -0500

    Made more static pages and ensured all of them had links to all other models.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 from app import views
--- app/static/jsx/years.js
+++ app/static/jsx/years.js
@@ -5,16 +5,16 @@ var Td = Reactable.Td;
 
 //How to use: https://github.com/glittershark/reactable
 var data = [
-    {Year: <a href="/years/1">1996</a>, 'Number of Games': '1','Most Popular Genre': 'Platform', 'Average Rating':'5.0', 'Number of Companies Founded': '1'},
-    {Year: <a href="/years/2">2001</a>, 'Number of Games': '1', 'Most Popular Genre': 'Platform', 'Average Rating':'4.2', 'Number of Companies Founded' : '1'},
-    {Year: <a href="/years/3">2003</a>, 'Number of Games': '1', 'Most Popular Genre': 'Action-Adventure', 'Average Rating':'4.9', 'Number of Companies Founded' : '0'}
+    {Year: <a href="/years/1">1996</a>, 'Number of Games': '1','Most Popular Genre': 'Platform', 'Average Rating':'5.0', 'Who Made Games': '1'},
+    {Year: <a href="/years/2">2001</a>, 'Number of Games': '1', 'Most Popular Genre': 'Platform', 'Average Rating':'4.2', 'Who Made Games' : '1'},
+    {Year: <a href="/years/3">2003</a>, 'Number of Games': '1', 'Most Popular Genre': 'Action-Adventure', 'Average Rating':'4.9', 'Who Made Games' : '0'}
 ];
 ReactDOM.render(
     <Table id="years-table" data={data}
            sortable={true}
            defaultSort={{column: 'Years', direction: 'desc'}}
-           filterable={['Year','Number of Games','Most Popular Genre','Average Rating', 'Number of Companies Founded']}
-           filterPlaceholder='Filter by Years, Number of Games, Most Popular Genre, Average Rating, or Number of Companies Founded'
+           filterable={['Year','Number of Games','Most Popular Genre','Average Rating', 'Who Made Games']}
+           filterPlaceholder='Filter by Years, Number of Games, Most Popular Genre, Average Rating, or Who Made Games'
            itemsPerPage={6} pageButtonLimit={10}
            defaultSortDescending />,
     document.getElementById('table')
