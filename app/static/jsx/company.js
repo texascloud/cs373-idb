55baab5e69b32dace3592455b5888970fbd90df6
CamelBackNotation <joshuahurt@utexas.edu>
2016-03-24 21:51:15 -0500

    Finished updating company data

--- app/static/jsx/company.js
+++ app/static/jsx/company.js
@@ -1,8 +1,11 @@
 
 var company_data = [
-    {Name: 'Naughty Dog', Developed: 60, Published: 581, Founded:'1974', 'Avg Rating':82*0.05},
-    {Name: 'THQ', Developed: 8, Published: 363, Founded:'1989', 'Avg Rating':77*0.05},
-    {Name: 'Obsidian Entertainment', Developed: 17, Published: 1, Founded:'2003', 'Avg Rating':81*0.05}
+    {Name: <a href="/companies/1"> Naughty Dog </a>, Developed: 60, Published: 581,
+        Founded:'1994', 'Avg Rating':82*0.05},
+    {Name: <a href="/companies/2"> Bungie </a>, Developed: 8, Published: 363,
+        Founded:'1989', 'Avg Rating':77*0.05},
+    {Name: <a href="/companies/3"> Bethesda Softworks</a>, Developed: 17, Published: 1,
+        Founded:'2003', 'Avg Rating':81*0.05}
 ];
 
 var game_data = [
--- app/static/jsx/year.js
+++ app/static/jsx/year.js
@@ -1,22 +1,25 @@
 var company_data = [
-    {Name: 'Naughty Dog', Developed: 60, Published: 581, Founded:'1974', 'Avg Rating':82*0.05},
-    {Name: 'THQ', Developed: 8, Published: 363, Founded:'1989', 'Avg Rating':77*0.05},
-    {Name: 'Obsidian Entertainment', Developed: 17, Published: 1, Founded:'2003', 'Avg Rating':81*0.05}
+    {Name: <a href="/companies/1"> Naughty Dog </a>, Developed: 60, Published: 581,
+        Founded:'1994', 'Avg Rating':82*0.05},
+    {Name: <a href="/companies/2"> Bungie </a>, Developed: 8, Published: 363,
+        Founded:'1989', 'Avg Rating':77*0.05},
+    {Name: <a href="/companies/3"> Bethesda Softworks</a>, Developed: 17, Published: 1,
+        Founded:'2003', 'Avg Rating':81*0.05}
 ];
 
 var game_data = [
     {Title: <a href="/games/1">Crash Bandicoot</a>, Genre: 'Platform', Console: 'PS1',
-        Developer: <a href="/companies/1">Naughty Dog</a>, Rating:'5.0', 'Release':<a href="/years/1996">1996</a>},
-    {Title: <a href="/games/2">Jak and Daxter</a>, Genre: 'Platform', Console: 'PS2',
-        Developer: <a href="/companies/1">Naughty Dog</a>, Rating:'4.2', 'Release':<a href="/years/2001">2001</a>},
-    {Title: <a href="/games/3">The Last of Us</a>, Genre: 'Action-Adventure, Survival-Horror', Console: 'PS3',
-        Developer: <a href="/companies/1">Naughty Dog</a>, Rating:'4.9', 'Release':<a href="/years/2003">2003</a>}
+        Developer: <a href="/companies/1">Naughty Dog</a>, Rating:'5.0', 'Release':<a href="/years/1">1996</a>},
+    {Title: <a href="/games/2">Halo: Combat Evolved</a>, Genre: 'Shooter', Console: 'Xbox',
+        Developer: <a href="/companies/1">Bungie</a>, Rating:'4.5', 'Release':<a href="/years/2">2001</a>},
+    {Title: <a href="/games/3">The Last of Us</a>, Genre: 'RPG', Console: 'PS3, Xbox 360',
+        Developer: <a href="/companies/1">Bethesda Softworks</a>, Rating:'4.9', 'Release':<a href="/years/3">2008</a>}
 ];
 
 var year_data = [
     {Year_id: 1, Year: <a href="/years/1">1996</a>, 'Number of Games': '1','Most Popular Genre': 'Platform', 'Average Rating':'5.0', 'Number of Companies Founded': '1'},
     {Year_id: 2, Year: <a href="/years/2">2001</a>, 'Number of Games': '1', 'Most Popular Genre': 'Platform', 'Average Rating':'4.2', 'Number of Companies Founded' : '1'},
-    {Year_id: 3, Year: <a href="/years/3">2003</a>, 'Number of Games': '1', 'Most Popular Genre': 'Action-Adventure', 'Average Rating':'4.9', 'Number of Companies Founded' : '0'}
+    {Year_id: 3, Year: <a href="/years/3">2008</a>, 'Number of Games': '1', 'Most Popular Genre': 'Action-Adventure', 'Average Rating':'4.9', 'Number of Companies Founded' : '0'}
 ];
 
 var YearInformation = React.createClass({
