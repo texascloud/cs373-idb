2e81a3db2a9d553bf730671e5b22c29193b4300b
CamelBackNotation <joshuahurt@utexas.edu>
2016-04-01 22:32:16 -0500

    Setup framework for restful API to close #29. Restructured project to split files into server and client directories to better label file roles.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from app import app
+from server import app, db
 
 # This is the run harness for our main application whose logic
-# is in the 'app' directory.
+# is in the 'server' directory.
+# app = setup_app()
 app.run(host='0.0.0.0', debug=True)
+
+# setup_app().run(host='0.0.0.0', debug=True)
--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from server.views import index
 
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+from server.bin_gamers_api import api
+app.register_blueprint(api.blueprint, url_prefix='/api')
+app.register_blueprint(index)
 db = SQLAlchemy(app)
-from app import views
