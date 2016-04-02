ff6740725af608636c4ac57445b552a309f5aa7e
Will Ripley <wwripley@gmail.com>
2016-04-02 14:32:06 -0500

    sqlMaker.sql table creation modified

--- igdbDataPull/sqlMaker.sql
+++ igdbDataPull/sqlMaker.sql
@@ -1,3 +1,29 @@
-CREATE DATABASE IF NOT EXISTS swe;
+DROP DATABASE IF EXISTS swe;
+CREATE DATABASE swe;
 use swe;
-CREATE TABLE  IF NOT EXISTS tblGames (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(30) NOT NULL, rating DEC(3,1), release_date DATE, PRIMARY KEY(id));
+
+CREATE TABLE tblGames (game_id INT NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(30) NOT NULL, image_url VARCHAR(255), 
+	rating DEC(4,2), release_date DATE, PRIMARY KEY(game_id));
+
+CREATE TABLE tblCompanies (company_id INT NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(30) NOT NULL, image_url VARCHAR(255), 
+	avg_rating DEC(4,2), date_founded DATE, PRIMARY KEY(company_id));
+
+CREATE TABLE tblYears (year_id INT NOT NULL, num_games INT,
+	most_popular_genre VARCHAR(40), PRIMARY KEY(year_id));
+
+CREATE TABLE tblGenre (genre_id INT NOT NULL AUTO_INCREMENT,
+	genre_name VARCHAR(30) NOT NULL, PRIMARY KEY(genre_id));
+
+CREATE TABLE tblConsoles (console_id INT NOT NULL AUTO_INCREMENT,
+	console_name VARCHAR(30) NOT NULL, PRIMARY KEY(console_id));
+
+CREATE TABLE tblGamesToCompaniesBridge (game_id INT NOT NULL, 
+	company_id INT NOT NULL, PRIMARY KEY(game_id, company_id));
+
+CREATE TABLE tblGamesToGenresBridge (game_id INT NOT NULL,
+	genre_id INT NOT NULL, PRIMARY KEY(game_id, genre_id));
+
+CREATE TABLE tblGamesToConsolesBridge (game_id INT NOT NULL,
+	console_id INT NOT NULL, PRIMARY KEY(game_id, console_id));
