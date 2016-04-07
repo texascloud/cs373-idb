541b54572bf515a9581051b4cd26b97397b5f857
CamelBackNotation <joshuahurt@utexas.edu>
2016-04-07 18:45:58 -0500

    Need to render more stuff for single pages still

--- client/components/year-page.jsx
+++ client/components/year-page.jsx
@@ -28,11 +28,20 @@ export default class YearPage extends React.Component {
     }
     else {
       let year = this.state.data[0];
+      let games = year.games_to_url;
+      console.log(games[0].name);
+      // var all_games = [];
+      // for (var g in year.games_to_url) {
+      //   all_games.add({'name': g, 'url':year.games_to_url[g]});
+      // }
       return (
         <div className="year-stats">
           <h2>Year: {id}</h2>
           <h2>Number of Games: <Link to="/games/">{year.num_games}</Link></h2>
           <h2>Most Popular Genre: {year.most_popular_genre}</h2>
+          <h3>{games.map((game) => {
+            <h1>{game.name}</h1>
+          })}</h3>
           <h2>Companies founded: <Link to="/companies/">{Object.keys(year.companies_to_url).length}</Link></h2>
         </div>
         );
