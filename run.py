aa8b3209625ca252732b05593edad41e1de21f9d
ios4ever <jerry.pon@utexas.edu>
2016-04-07 19:47:34 -0500

    Attempt to pass on travis

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ from server import app
 # This is the run harness for our main application whose logic
 # is in the 'server' directory.
 app.run(host='0.0.0.0', debug=True)
+
+def create_test_db():
+	app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://travis@localhost/swe_test'
+   	db.create_all()
