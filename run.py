8dd18a0f5cf70b7d458c60ef96cb4589d35ab839
CamelBackNotation <joshuahurt@utexas.edu>
2016-04-07 21:40:49 -0500

    Master was too far out of date. Our bad. Merge conflicts put us in danger of scrubbing too many changes

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from server import app, db
-
-# This is the run harness for our main application whose logic
-# is in the 'server' directory.
-# app = setup_app()
-app.run(host='0.0.0.0', debug=True)
-
-# setup_app().run(host='0.0.0.0', debug=True)
